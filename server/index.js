923aecdd4677d27a569ecf146d9319e8daf81d92
Amar Yadav <amaredu626@gmail.com>
2024-07-02 11:10:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const emailToSocketIdMap = new Map();
 const socketidToEmailMap = new Map();
 
 io.on("connection", (socket) => {
-  console.log(Socket Connected, socket.id);
+  
   socket.on("room:join", (data) => {
     const { email, room } = data;
     emailToSocketIdMap.set(email, socket.id);
